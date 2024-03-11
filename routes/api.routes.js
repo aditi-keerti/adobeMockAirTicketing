470da5d4efd7c12cdd4ccf4335989766b62093fb
aditi-keerti <aditibhadoriya1333@gmail.com>
2024-03-11 15:40:08 +0530

    Features:flights routs complete

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -1,5 +1,6 @@
 const express=require('express');
 const {UserModel}=require('../modals/user.model');
+const {FlightModel}=require('../modals/flight.model');
 const bcrypt=require('bcrypt')
 const api=express.Router();
 
@@ -34,3 +35,55 @@ api.post('/login',async(req,res)=>{
         res.status(500).json(err);
     }
 })
+
+api.get('/flights',async(req,res)=>{
+ try{
+    const flights=await FlightModel.find();
+    if(!flights||flights.length===0){
+        return res.status(404).json({err:"No flight Found"});
+    }
+    res.status(200).json(flights);
+ }catch(err){
+    res.status(400).json(err);
+ }
+})
+
+api.get('flights/:id',async(req,res)=>{
+    const flightId=req.params.id;
+    try{
+        const flight=await FlightModel.findById(flightId);
+        if(!flight){
+            return res.status(404).json({err:"Flight Not Found"});
+        }
+        res.status(200).json(flight);
+    }catch(err){
+        res.status(500).json({err:"Internal server Error"});
+    }
+});
+
+api.patch('flights/:id',async(req,res)=>{
+  const flightId=req.params.id;
+  const updatedData=req.body;
+  try{
+    const flight=await FlightModel.findByIdAndUpdate(flightId,updatedData,{new:true});
+    if(!flight){
+        return res.status(404).json({err:"Flight Not Found"});
+    }
+    res.status(200).json(flight);
+  }catch(err){
+    res.status(500).json({err:"Internal Server error"});
+  }
+})
+api.delete('flights/:id',async(req,res)=>{
+    const flightId=req.params.id;
+   
+    try{
+      await FlightModel.findByIdAndDelete(flightId,updatedData,{new:true});
+     
+      res.status(200).json({msg:"Flight deleted"});
+    }catch(err){
+      res.status(500).json({err:"Internal Server error"});
+    }
+  })
+
+  
